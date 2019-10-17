519c8e43bc2ec77c4e296d8a44483a2304755cdd
delinskijc <delinskijc@vcu.edu>
2019-10-16 23:16:52 -0400

    Added HomeCalculator Activity

---
+++ app/src/main/java/com/example/cmsc_355_g18_csbuddy/HomeCalculator.java
@@ -0,0 +1,14 @@
+package com.example.cmsc_355_g18_csbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_calculator);
+    }
+}
