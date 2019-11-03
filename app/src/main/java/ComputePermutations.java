5f971dda67fe6c4f6b0f4b8c90624109cdb70d1f
Mark Groves <markgroves757@gmail.com>
2019-11-03 11:54:14 -0500

    Test commit of main functions

--- app/src/main/java/ComputePermutations.java
+++ app/src/main/java/ComputePermutations.java
@@ -1,8 +1,46 @@
+import android.os.Bundle;
+import Java.util.Scanner;
+
 /*
 /  Author: Mark Groves
-/  Version: 10/9/19
+/  Init Date: 10/9/19
 */
 
+
 public class ComputePermutations {
+
+    public static void main(String[] args) {
+        boolean repeat;
+
+        // C or P choice from user
+        // maybe replace with buttons
+        Scanner in = new Scanner(System.in);
+        String user_in = in.next();
+
+        // comb
+        if (user_in.equals("C")){
+            // repeat?
+        }
+
+        // perm
+        else if(user_in.equals("P")){
+            // repeat?
+        }
+
+        else {
+            // try again
+        }
+    }
+
+    private static int factorial(int n) {
+        if(n<=1){
+            return 1;
+        }
+        return (n * factorial(n-1));
+    }
+
+    private static int nPr(int n, int r) {
+        return (factorial(n) / factorial(n - r));
+    }
 
 }
