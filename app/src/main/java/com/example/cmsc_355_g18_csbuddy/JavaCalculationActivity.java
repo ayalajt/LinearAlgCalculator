10ae25a97b4e53550ed91636fd773b65a6718947
stitzrp <stitzrp@gmail.com>
2019-11-14 22:53:25 -0500

    added the file with some starting java code for the spinners

--- app/src/main/java/com/example/cmsc_355_g18_csbuddy/JavaCalculationActivity.java
+++ app/src/main/java/com/example/cmsc_355_g18_csbuddy/JavaCalculationActivity.java
@@ -3,6 +3,7 @@ package com.example.cmsc_355_g18_csbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.os.Bundle;
@@ -20,7 +21,13 @@ import androidx.lifecycle.Observer;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class JavaCalculationActivity extends Fragment {
+
+    Spinner primitiveTypesSpinner1;
+    Spinner primitiveTypesSpinner2;
 
     @Override
     public View onCreateView(
@@ -28,20 +35,20 @@ public class JavaCalculationActivity extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.activity_java_calculation, container, false);
 
-        //Creates a new spinner for the primitive data type options associated with the first operand number
-        Spinner primitiveType1 = (Spinner)root.findViewById(R.id.spinner1);
-        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(getContext(),
-                R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.dataTypes));
-        arrayAdapter1.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        primitiveType1.setAdapter(arrayAdapter1);
+        List<JavaArithmetic.DataTypes> primitveDataTypes = Arrays.asList(JavaArithmetic.DataTypes.values());
 
-        //Creates a new spinner for the primitive data type options associated with the second operand number
-        Spinner primitiveType2 = (Spinner)root.findViewById(R.id.spinner2);
-        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(getContext(),
-                R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.dataTypes));
-        arrayAdapter2.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        primitiveType2.setAdapter(arrayAdapter2);
+        //makes a spinner containing the values from DataTypes enum contained in JavaArithmetic
+        primitiveTypesSpinner1 = root.findViewById(R.id.spinner1);
+        ArrayAdapter<JavaArithmetic.DataTypes> adapter1 = new ArrayAdapter<JavaArithmetic.DataTypes>(this.getActivity(),
+                android.R.layout.simple_spinner_item, primitveDataTypes);
+        adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        primitiveTypesSpinner1.setAdapter(adapter1);
 
+        primitiveTypesSpinner2 = root.findViewById(R.id.spinner2);
+        ArrayAdapter<JavaArithmetic.DataTypes> adapter2 = new ArrayAdapter<JavaArithmetic.DataTypes>(this.getActivity(),
+                android.R.layout.simple_spinner_item, primitveDataTypes);
+        adapter2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        primitiveTypesSpinner2.setAdapter(adapter2);
 
         return root;
     }
