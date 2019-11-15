d2c8a4a47f13a72329c9d621412ebb905d6da1c3
stitzrp <stitzrp@gmail.com>
2019-11-14 23:59:15 -0500

    last commit prior to deadline

--- app/src/main/java/com/example/cmsc_355_g18_csbuddy/JavaCalculationActivity.java
+++ app/src/main/java/com/example/cmsc_355_g18_csbuddy/JavaCalculationActivity.java
@@ -16,6 +16,7 @@ import android.view.LayoutInflater;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 public class JavaCalculationActivity extends Fragment {
 
@@ -32,61 +33,32 @@ public class JavaCalculationActivity extends Fragment {
         View root = inflater.inflate(R.layout.activity_java_calculation, container, false);
 
         List<JavaArithmetic.DataTypes> primitveDataTypes = Arrays.asList(JavaArithmetic.DataTypes.values());
+        ArrayList<String> primitiveDataTypeStrings = new ArrayList<String>();
+        for (JavaArithmetic.DataTypes t: primitveDataTypes) {
+            primitiveDataTypeStrings.add(t.toString());
+        }
 
-        //makes a spinner containing the values from DataTypes enum contained in JavaArithmetic
         primitiveTypesSpinner1 = root.findViewById(R.id.spinner1);
         ArrayAdapter<JavaArithmetic.DataTypes> adapter1 = new ArrayAdapter<JavaArithmetic.DataTypes>(this.getActivity(),
                 android.R.layout.simple_spinner_item, primitveDataTypes);
-        adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         primitiveTypesSpinner1.setAdapter(adapter1);
         primitiveTypesSpinner1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                switch(i) {
-                    case 0:
-                        t1 = JavaArithmetic.DataTypes.DOUBLE;
-                        break;
-                    case 1:
-                        t1 = JavaArithmetic.DataTypes.FLOAT;
-                        break;
-                    case 2:
-                        t1 = JavaArithmetic.DataTypes.INT;
-                        break;
-                    case 4:
-                        t1 = JavaArithmetic.DataTypes.SHORT;
-                        break;
-                    default:
-                        t1 = JavaArithmetic.DataTypes.LONG;
-                        break;
-                }
+                t1 = (JavaArithmetic.DataTypes)adapterView.getItemAtPosition(i);
             }
         });
 
         primitiveTypesSpinner2 = root.findViewById(R.id.spinner2);
         ArrayAdapter<JavaArithmetic.DataTypes> adapter2 = new ArrayAdapter<JavaArithmetic.DataTypes>(this.getActivity(),
                 android.R.layout.simple_spinner_item, primitveDataTypes);
-        adapter2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         primitiveTypesSpinner2.setAdapter(adapter2);
         primitiveTypesSpinner2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                switch(i) {
-                    case 0:
-                        t2 = JavaArithmetic.DataTypes.DOUBLE;
-                        break;
-                    case 1:
-                        t2 = JavaArithmetic.DataTypes.FLOAT;
-                        break;
-                    case 2:
-                        t2 = JavaArithmetic.DataTypes.INT;
-                        break;
-                    case 4:
-                        t2 = JavaArithmetic.DataTypes.SHORT;
-                        break;
-                    default:
-                        t2 = JavaArithmetic.DataTypes.LONG;
-                        break;
-                }
+                t2 = (JavaArithmetic.DataTypes)adapterView.getItemAtPosition(i);
             }
         });
 
